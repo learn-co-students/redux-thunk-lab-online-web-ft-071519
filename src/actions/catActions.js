c3c790dfe005eba5bf0e6c125100df141db90ff9
Nicole Dow <nikki.dow71@gmail.com>
2019-12-04 14:16:28 -0700

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,18 +1,10 @@
-// write and export your action creator function here
-// fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
-//   return response.json()
-// }).then(responseJSON => {
-//   // instead of logging here, call dispatch and send the cat JSON data to your store
-//   console.log(responseJSON.images)
-// })
-
-export function fetchCats() {
+export const fetchCats = () => {
     return (dispatch) => {
-        dispatch({ type: 'LOADING_CATS'});
-        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
-          .then(response => response.json())
-          .then(responseJSON => {
-              dispatch({type: 'ADD_CATS', cats: responseJSON.images})
-          })
+      dispatch({ type: 'LOADING_CATS'})
+      fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+        return response.json()
+      }).then(responseJSON => {
+        dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+      })
     }
-}
+  }
