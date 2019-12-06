20d01c9478573e4f954d42ab055735bc713fed12
radville <laura.radville@gmail.com>
2019-12-06 09:51:52 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class CatList extends Component {
     render() {
         return (
-            this.props.catPics.map(pic =>
-                <img src={pic.url} />
-            )
+            <div>
+                {this.props.catPics.map(pic =>
+                    <img src={pic.url} key={pic.id}/>
+                )}
+            </div>
         )
     }
 }
