891286944973508a064bec2b4eaab41cc7da6eba
BaronLip <baronlip@gmail.com>
2019-12-07 18:46:57 -0600

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,16 @@
 // write your CatList component here
+import React, { Component } from 'react'
+
+class Catlist extends Component {
+    render() {
+        console.log(this.props)
+        const catPics = this.props.catPics.map(cat => <img src={cat.url} height="100" width="100"/>)
+        return(
+            <div>
+                {catPics}        
+            </div>
+        )
+    }
+}
+
+export default Catlist
--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,1 +1,29 @@
-// add the catsReducer
+/*
+Build out the reducer. Add cases as you build.
+
+The Reducer:
+1. Sets the initial state of the app.
+2. Is a function that uses switch statements to determine which block of code to activate. Its purpose and return values change the state/store of the app. Changes in state will cause a re-render and therefore update the browser window based on the information in state.
+*/
+
+const catsReducer = (state = { cats: [], loading: false }, action) => {
+    switch (action.type) {
+        case 'LOADING_CATS':
+            return {
+                ...state,
+                cats: [...state.cats],
+                loading: true
+            }
+        case 'ADD_CATS':
+            return {
+                ...state,
+                cats: action.cats,
+                loading: false
+            }
+        default:
+            return state;
+    }
+}
+
+// exporting the function or class allows other components to import it. 
+export default catsReducer
